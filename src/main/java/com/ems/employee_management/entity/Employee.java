d3191f0e4d839a538244091dab89845ebc99ff80
venkatesh <mekalav159@gmail.com>
2026-02-25 05:52:37 +0530

    Entity and repository added

---
+++ src/main/java/com/ems/employee_management/entity/Employee.java
@@ -0,0 +1,22 @@
+package com.ems.employee_management.entity;
+import lombok.Data;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
